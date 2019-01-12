60639c864dd5b63972a85feda1f0c92532398a86
chrkhl <kuehlchristian@icloud.com>
2019-01-12 12:28:42 +0100

    prepare cli tool

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+#!/usr/bin/env node
+'use strict';
+
 const connector = require('./sphero-connector');
 const startServer = require('./server');
 
-startServer(connector);
+(async() => {
+  await connector.connectSpheroMini();
+  connector.setMainLedColor('#FFFFFF');
+  startServer(connector);
+})();
