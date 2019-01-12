890dcfdfda4bfd3cc4dfcc5845fd105980a8daec
chrkhl <kuehlchristian@icloud.com>
2019-01-12 14:12:16 +0100

    adds config and updater

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 #!/usr/bin/env node
 'use strict';
 
+const pkg = require('./package.json');
+const updateNotifier = require('update-notifier');
 const connector = require('./sphero-connector');
 const startServer = require('./server');
+const cosmiconfig = require('cosmiconfig');
+
+updateNotifier({ pkg }).notify();
 
 (async() => {
+  const foundConfig = await cosmiconfig('sphero-connector-ipc', { searchPlaces: [ 'package.json' ] }).search();
+
+  if (!foundConfig) {
+    return console.error('config not found!');
+  }
+
+  const connectorConfig = foundConfig.config;
+  console.log('config stuff', connectorConfig);
+
   await connector.connectSpheroMini();
   connector.setMainLedColor('#FFFFFF');
   startServer(connector);
